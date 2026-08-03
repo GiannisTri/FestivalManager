a05ee1cd9647a5d61e43219842c4b1c2e760d474
GiannisTri <giannistriantafyllidhs67@gmail.com>
2026-08-03 12:25:00 +0300

    Remove login redirection in error handling
    Remove redirection to login page on error.

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -34,7 +34,7 @@ api.interceptors.response.use(
       localStorage.removeItem("token");
       localStorage.removeItem("user");
 
-      window.location.href = "/login";
+      
     }
 
     return Promise.reject(error);
